2abcb836a62b3c916b6fa58d50cc70daff45bd75
nknilandu <nknilandu@gmail.com>
2026-08-07 01:17:25 +0600

    added create user api to save data

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 // Replace the placeholder with your Atlas connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hgpfzti.mongodb.net/?appName=Cluster0`;
@@ -27,22 +25,36 @@ async function runStableAPIConnect() {
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("bookVerse");
+    const users = database.collection("users");
+    // =============================
+
+    // CREATE USER
+    // ============================
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      if (!user.email) {
+        return res.status(400).send({ message: "Error! User mail not found" });
+      }
+      const result = await users.insertOne(user);
+      return res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    const result = await client.db('admin').command({ ping: 1 });
+    const result = await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
+      "Pinged your deployment. You successfully connected to MongoDB!",
     );
     return result;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
   }
 }
 runStableAPIConnect().catch(console.dir);
 // =======================================
 // Routes
-app.get('/', (req, res) => {
-  res.send('Book Verse Server is running!');
+app.get("/", (req, res) => {
+  res.send("Book Verse Server is running!");
 });
 
 // Start server
